86b3b8dac62eb5d4c62d7be36a35349372fe7101
k1590 <kevinbiju1590@gmail.com>
2026-06-14 00:37:29 +0530

    Fix single card layout - remove empty box artifact

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -36,28 +36,7 @@ const WhatIDo = () => {
       </div>
       <div className="what-box">
         <div className="what-box-in">
-          <div className="what-border2">
-            <svg width="100%">
-              <line
-                x1="0"
-                y1="0"
-                x2="0"
-                y2="100%"
-                stroke="white"
-                strokeWidth="2"
-                strokeDasharray="7,7"
-              />
-              <line
-                x1="100%"
-                y1="0"
-                x2="100%"
-                y2="100%"
-                stroke="white"
-                strokeWidth="2"
-                strokeDasharray="7,7"
-              />
-            </svg>
-          </div>
+
           <div
             className="what-content what-noTouch"
             ref={(el) => setRef(el, 0)}
